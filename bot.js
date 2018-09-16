053c23a06cb83819046d3de79c77111108858823
barbarbar338 <35371155+barbarbar338@users.noreply.github.com>
2018-09-16 13:52:32 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+  
+  if (msg.content === "-kabulet") {
+	  msg.author.send("Kuralları okumuş olduğunu kabul ettin ve artık `@Başlangıç Seviyesi` rolüne sahipsin!")
+	  let role = msg.guild.roles.find("name", "Başlangıç Seviyesi");
+	  msg.member.addRole(role.id);
+	  
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
