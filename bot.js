ce44bf1603db443623f0e57cdee9628d22d8f304
eugeneparker <43089803+eugeneparker@users.noreply.github.com>
2018-09-18 17:30:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,23 +2,17 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+	console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-	if (!msg.channel.id === "489825901241171968")   return msg.reply("Bu komut sadece <#490826053917474826> odasında kullanılabilir!");
-		  if (msg.content === "-kabulet") {
-			  
-	  	  let role = msg.guild.roles.find("name", "Başlangıç Seviyesi");
-	  msg.member.addRole(role.id);
-	  msg.author.send("Kuralları okumuş olduğunu kabul ettin ve artık `@Başlangıç Seviyesi` rolüne sahipsin!")
-
-	  
-  }
-
- 
-
-
+	if (msg.content === "-kabulet") {
+		
+		let role = msg.guild.roles.find("name", "Başlangıç Seviyesi");
+		
+		msg.member.addRole(role.id);
+		msg.author.send("Kuralları okumuş olduğunu kabul ettin ve artık `@Başlangıç Seviyesi` rolüne sahipsin!")
+	}
 });
 
 client.login(process.env.BOT_TOKEN);
